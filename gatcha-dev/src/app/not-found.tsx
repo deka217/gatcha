8af8d3a8434668ee58810c94bb55d9b70377abfb
Mas Dhody <49305784+deka217@users.noreply.github.com>
2024-05-02 00:23:10 +0700

    Delete gatcha-dev directory

--- gatcha-dev/src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-const NotFound = () => {
-    return (
-        <>
-        Not Found bang
-        </>
-    )
-}
-export default NotFound
--- gatcha-dev/src/components/footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="text-sm text-center">Copyright&copy; 2024 yoshid.com</div>
-    )
-}
-
-export default Footer
